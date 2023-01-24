cce6b1c5015f18a09cafbffc10f83100e16c41ed
Rodrigo Fernandes <51092062+rodozinho@users.noreply.github.com>
2023-01-24 11:36:52 -0300

    Update ferraz.R

--- ferraz.R
+++ ferraz.R
@@ -1,4 +1,4 @@
-# it is useful to get to know the paper being replicated here as I won't explain every step taken there. However, in general, the authors use an exogenous cap created by the brazilian Supreme Court to understand how campaing spendings affect political competition.
+# it is useful to get to know the paper being replicated here as I won't explain every step taken. However, in general, the authors use an exogenous cap created by the brazilian Supreme Court to understand how campaing spendings affect political competition.
 
 ### Importing all (and more) required packages
 
@@ -6,8 +6,13 @@ if (!require("pacman")) install.packages("pacman")
 p_load(ggplot2,tidyverse,fixest,vroom,SciViews,scales,caret, cowplot,data.table,deflateBR,
        modelsummary,openxlsx,vtable,readxl,rdd, jtools,gridExtra)
 
-# Defining a function to trim outliers. you can choose the p according to your research design
-w = function(x,p=0.01) psych::winsor(x = x, trim = p)
+# Defining a function to trim outliers. you can choose the quantile according to your research design
+qrep <- function(x){ 
+  quantiles <- quantile(x, c(.01, .99 ),na.rm=T)
+  x[x < quantiles[1]] <- quantiles[1]
+  x[x > quantiles[2]] <- quantiles[2]
+  x
+}
 
 # Cleaning environment
 rm(list=ls())
@@ -20,5 +25,18 @@ dado <- dado %>% filter(year >= 2006) # cleaning the data a little
 dado$post <-ifelse(dado$year <= 2016, 0,1) # for the RDD, we will need a dummy marking the year
 
 ln <- dado %>% select(starts_with("nr")) %>% ln() %>%
-  rename_with(~paste0("ln","_",.x)) # ln nas variaveis comecando com nr
-dado <- cbind(dado,ln) #juntando
+  rename_with(~paste0("ln","_",.x)) # ln on variables starting with "nr"
+dado <- cbind(dado,ln)
+
+dado$muni_expenses <- 0
+
+dado <- dado %>%
+  rowwise() %>%
+  mutate(muni_expenses = sum(c_across(ends_with("finbra")), na.rm = F)) # sum of finbra
+
+
+ln <- dado %>% select(ends_with("finbra")) %>% ln() %>%
+  rename_with(~paste0("ln","_",.x)) #ln on variables starting with "finbra"
+dado <- cbind(dado,ln)
+
+rm(ln) # cleaning the environment
