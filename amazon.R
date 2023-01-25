b023d32237cf3b0849f33e02520457fe23a4c04c
Rodrigo Fernandes <51092062+rodozinho@users.noreply.github.com>
2023-01-25 11:48:54 -0300

    Update amazon.R

--- amazon.R
+++ amazon.R
@@ -3,7 +3,7 @@
 if (!require("pacman")) install.packages("pacman")
 p_load(ggplot2,deflateBR, readxl, tidyverse, lubridate, sf, zoo, data.table, factoextra, ggbiplot, magrittr, grid,
        rnaturalearth, rnaturalearthdata, rgeos,car,SciViews,scales,caret,vroom,ggpattern,maps,geobr,maptools,FNN,plm,
-       fixest,modelsummary,vtable,plm,did,Matching,formattable,sf,rgeos,MatchIt,devtools,DAPSm,did2s,cowplot)
+       fixest,modelsummary,vtable,plm,did,Matching,formattable,sf,rgeos,MatchIt,devtools,DAPSm,did2s,cowplot,ggmap)
 
 # Cleaning the environment
    
@@ -51,6 +51,42 @@ base <- left_join(base,area_mun, by = "code_muni")
 
 rm(list=setdiff(ls(), "base")) 
 
+# areas of conservation units/indigenous territory per year, for each municipality
+
 UC_area <- read_excel("UC_TI_area.xlsx")
 UC_area <- UC_area %>% filter(amz_legal==1)
 UC_area$ano <- as.numeric(UC_area$ano)
+
+UC_area<-aggregate(area_kmq ~ code_muni + ano, data=UC_area, FUN=sum) # sum all territories for each municipality and every year
+UC_area<- UC_area%>%
+  group_by(code_muni,ano)%>%
+  mutate(cumsum=cumsum(area_kmq))
+
+options(scipen = 999)
+UC_area <- UC_area %>%  dplyr::select(-area_kmq)
+UC_area <- UC_area %>% dplyr::rename(area_prot = cumsum)
+
+base <- left_join(base, UC_area, by = c("code_muni","ano"))
+base <- base %>% mutate_at(vars(c(area_prot)), ~replace(., is.na(.), 0))
+
+rm(list=setdiff(ls(), "base"))
+
+# GDP per capita for each municipality
+pib_per_capita <- read_excel("pib_per_capita.xlsx") 
+
+pib_per_capita$data <- as.Date(paste(pib_per_capita$ano, 12, 31, sep = "-"))  # criando data pra deflacionar
+pib_per_capita$pib_per_deflac <- deflate(pib_per_capita$pib_per_capita,
+                                         pib_per_capita$data, real_date = "01/2020")
+#pib_per_capita <- pib_per_capita %>% select(-pib_capita_corrente,-data)
+base  <- left_join(base, pib_per_capita, by = c("ano","code_muni")) #juntando na base dnv
+rm(list=setdiff(ls(), "base"))
+
+### how to create a map? get coordenates at https://www.openstreetmap.org/export#map=14/-22.9645/-43.193 This method is my favorite since it's pretty straightforward, easy to visualizate the boundaries wished (just limit it as you wish at openstreetmap) and it has many maptypes. Either way, below I will also exlopre other ways.
+
+nig_map <- get_stamenmap( #getting Nigeria's map coord 
+  bbox = c(left = -2.8,bottom = 1.31,right=20.2,top = 16.5),
+  maptype = "terrain",zoom=6
+)
+ggmap(nig_map)+geom_point(data= subs,aes(x = lon,y=lat,color=indicator))+scale_color_viridis_c(option = "magma")+
+  theme_map()+labs(title="Figure 2") 
+
