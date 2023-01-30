6643fc586dafc07434b1f18240828ba19dd7febc
Rodrigo Fernandes <51092062+rodozinho@users.noreply.github.com>
2023-01-30 11:24:06 -0300

    Update campaign_cap.R

--- campaign_cap.R
+++ campaign_cap.R
@@ -9,14 +9,6 @@ p_load(ggplot2,tidyverse,fixest,vroom,SciViews,scales,caret, cowplot,data.table,
 wd <- "C:/example/" # setting wd
 setwd(dir)
 
-# Defining a function to trim outliers. you can choose the quantile according to your research design
-qrep <- function(x){ 
-  quantiles <- quantile(x, c(.01, .99 ),na.rm=T)
-  x[x < quantiles[1]] <- quantiles[1]
-  x[x > quantiles[2]] <- quantiles[2]
-  x
-}
-
 # Cleaning the environment
 rm(list=ls())
 gc()
@@ -44,7 +36,7 @@ dado <- cbind(dado,ln)
 
 rm(ln) # cleaning the environment
 
-dado$ln_gdp <- ln(dado$gdp)
+dado$ln_gdp <- ln(dado$gdp) # setting some variables of interest
 dado$ln_muni_expenses <- ln(dado$muni_expenses)
 dado$ln_empl <- ln(dado$nr_empl)
 
@@ -59,7 +51,7 @@ dado$frac <-  (dado$health_finbra+dado$education_finbra)/dado$muni_expenses # sp
 
 dado$diff_votos <- log(dado$votos_primeiro_colocado/dado$votos_segundo_colocado) # difference of votes between the first and the second candidate (our measure of political competition)
 
-dado[] <- Map(function(x) replace(x, is.infinite(x), 0), dado) # replacing inf from the ln to zero
+dado[] <- Map(function(x) replace(x, is.infinite(x), 0), dado) # replacing inf from the ln to zero. it isn't wrong since they were 0 by default
 
 dat2 <- dado %>% select(starts_with("ln_"),frac,burocratic,diff_votos) %>% qrep(.) # applying the outliers function
             
@@ -75,17 +67,23 @@ feols(nr_candidatos ~ higher_spending_cap + diff:i(higher_spending_cap)|uf, data
 feols(nr_candidatos ~ higher_spending_cap + diff2:i(higher_spending_cap)+diff:i(higher_spending_cap)| uf, 
       data = subdado %>% dplyr::mutate(diff2=diff*diff) , cluster = subdado$ibge)
               
-ggplot(data=subdado %>% filter(year==2017,!is.na(higher_spending_cap)), # plotting
-aes(y=nr_candidatos))+ ggtitle("Dummy higher_spending_cap") +
+ggplot(data=subdado %>% select(nr_candidatos,higher_spending_cap,year) %>% filter(year==2017,!is.na(higher_spending_cap))
+       %>% mutate(across(c(1:3),qrep)), # plotting
+       aes(y=nr_candidatos))+ ggtitle("Number of candidates for higher spending cap") +
   geom_boxplot()+facet_grid(~higher_spending_cap) + theme(axis.title.x=element_blank(),
-                                                                                     axis.text.x=element_blank(),
-                                                                                     axis.ticks.x=element_blank())
-              
+                                                          axis.text.x=element_blank(),
 
+# Defining a function to trim outliers. you can choose the quantile according to your research design
+qrep <- function(x){ 
+  quantiles <- quantile(x, c(.01, .99 ),na.rm=T)
+  x[x < quantiles[1]] <- quantiles[1]
+  x[x > quantiles[2]] <- quantiles[2]
+  x
+}
 ##### Now that I've replicated the paper, let's try to expand it. Is there some relationship between political competition and environmental issues? ------
 
 # First, we will try to understand how competition affects emission, for all municipalities. 
-vroom("emissoes.csv") -> emissoes
+emissoes <- vroom("emissoes.csv")
 subdado <- subdado %>% left_join(.,emissoes,by=c("year","ibge"))
 
 feols(log(emissao_co2) ~ higher_spending_cap:post| 
@@ -93,10 +91,10 @@ feols(log(emissao_co2) ~ higher_spending_cap:post|
            ibge+ meso^year,
            ibge + micro^year), data = subdado,
       cluster = "ibge") -> a
+                                                          
 b<-modelsummary(a,output="data.frame",stars=TRUE)
 addWorksheet(wb, "emissao")
 writeData(wb, "emissao", b, startRow = 1, startCol = 1)
 saveWorkbook(wb, file = paste0(wd,Sys.Date(),"_results.xlsx"), overwrite = TRUE)
 
-# What if we want to see only for municipalities inside the Legal Amazon?
  
