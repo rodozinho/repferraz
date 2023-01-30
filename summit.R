e86b2f5f839e24f22a1edb3d1b0ad38c29d63a0f
rodozinho <rofegon@gmail.com>
2023-01-30 15:03:28 -0300

    comm

--- summit.R
+++ summit.R
@@ -1,5 +1,5 @@
 # in this file, I will try to see if there is some relationship between environmental conferences/summit and the raise of a marxist view on environmental issues.
-# my hypothesis is that by seeing how incable governments are currently to deal with the demanding issue regarding climate change, people react to that by adopting more radical perspectives.
+# my hypothesis is that by seeing how incapable governments are currently to deal with the demanding issue regarding climate change, people react to that by adopting more radical perspectives.
 # using textual analysis, webscrapping and machine learning.
-# I will also be using it to understand how to synch R and GitHub.
+# I will also be using it to understand how to sync R and GitHub.
 
